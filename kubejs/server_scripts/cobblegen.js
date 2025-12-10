a75d82a7a52434dc38f71fb50f489bab12a33cad
Elle <115524533+hexagonelle@users.noreply.github.com>
2025-12-10 13:07:15 -0800

    optimization mods, added glowing obsididan

--- kubejs/server_scripts/cobblegen.js
+++ kubejs/server_scripts/cobblegen.js
@@ -1,18 +1,18 @@
 LootJS.modifiers(event => {
 
-	const cryingWhenSilkTouch =
-		LootEntry.
-			of("minecraft:crying_obsidian").
-			when(c =>
-				c.matchMainHand(
-					ItemFilter.hasEnchantment("minecraft:silk_touch")
-				)
-			);
+	// const cryingWhenSilkTouch =
+	// 	LootEntry.
+	// 		of("minecraft:crying_obsidian").
+	// 		when(c =>
+	// 			c.matchMainHand(
+	// 				ItemFilter.hasEnchantment("minecraft:silk_touch")
+	// 			)
+	// 		);
 
 	const obsidian = "minecraft:obsidian";
 
 	event
-		.addBlockLootModifier("minecraft:crying_obsidian")
+		.addBlockLootModifier("kubejs:glowing_obsidian")
 		.removeLoot(Ingredient.all)
-		.addAlternativesLoot(cryingWhenSilkTouch, obsidian);
+		.addAlternativesLoot(obsidian);
 });
