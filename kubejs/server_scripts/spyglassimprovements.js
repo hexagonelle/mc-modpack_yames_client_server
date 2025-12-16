d0daeda84104d64d28bcdc446bd4bdfb01d65cfe
Elle <115524533+hexagonelle@users.noreply.github.com>
2025-12-15 16:13:06 -0800

    Fixes
    Added nerb, made spyglass belt-compatible, removed tectonic (possible server tick issue)

---
+++ kubejs/server_scripts/spyglassimprovements.js
@@ -0,0 +1,9 @@
+ServerEvents.tags('item', event => {
+	// Add a forge tag for unstripped logs
+	// This way recipes can distinguish between unstripped and stripped
+
+	
+	event.add('curios:belt', "minecraft:spyglass")
+	
+	
+})
