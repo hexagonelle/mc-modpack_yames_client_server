92f350b5b30ad326a9514e19d661e062a5e26e06
Elle <115524533+hexagonelle@users.noreply.github.com>
2026-01-09 12:29:29 -0800

    Updated mods & scripts
    Fixed some create crushing recipes for resources from stonetypes

--- kubejs/server_scripts/create.js
+++ kubejs/server_scripts/create.js
@@ -5,6 +5,17 @@ ServerEvents.recipes(event => {
 	// remove create tuff crushing
 	event.remove({ id:"create:crushing/tuff" })
 	event.remove({ id:"create:crushing/tuff_recycling" })
+    
+    //remove scoria & scorchia recipes,
+    //to make resources from them rarer
+	event.remove({ id:"create:crafting/palettes/scorchia" })
+	event.remove({ id:"create:smelting/scoria" })
+    
+    //redundant recipe
+	event.remove({ id:"create:crushing/asurine" })
+	event.remove({ id:"create:crushing/crimsite" })
+	event.remove({ id:"create:crushing/ochrum" })
+	event.remove({ id:"create:crushing/veridium" })
 
 	// Splashing recipe to turn bone blocks into calcite
 	// Purely for convenience
@@ -19,7 +30,7 @@ ServerEvents.recipes(event => {
 			'minecraft:flint',
 			Item.of('minecraft:netherite_scrap').withChance(0.001)
 		],
-		'create:scoria'
+		'#create:stone_types/scoria'
 	).processingTime(1000)
 
 	// Recipe to obtain ancient debris via compacting
@@ -27,7 +38,7 @@ ServerEvents.recipes(event => {
 		['minecraft:ancient_debris'],
 		[
 			Fluid.lava(100),
-			Item.of('minecraft:netherite_scrap',2),
+			Item.of('minecraft:netherite_scrap',4),
 			Item.of('create:scoria',8)
 		]
 	).superheated()
--- kubejs/server_scripts/create_embers.js
+++ kubejs/server_scripts/create_embers.js
@@ -4,12 +4,16 @@ ServerEvents.recipes(event => {
 	event.remove({id:"embers:copper_nugget_to_ingot"})
 
 	// get silver from asurine
+	event.remove({id:"create:crushing/asurine_recycling"})
 	event.recipes.create.crushing(
 		[
+			Item.of('create:crushed_raw_zinc').withChance(0.3),
+			Item.of('create:zinc_nugget').withChance(0.3),
+			Item.of('create:crushed_raw_silver').withChance(0.05),
 			Item.of('embers:silver_nugget').withChance(0.05)
 		],
-		'create:asurine'
-	).processingTime(500)
+		'#create:stone_types/asurine'
+	).processingTime(500).id("create:crushing/asurine_recycling")
 
 	// remove embers plate-crafting with hammer
 
